c1f4512d6ac784d03db8eb776a2790c93dc2c35f
chandu <chandugadeshwer@gmail.com>
2025-03-02 12:26:51 +0530

    Removed sensitive data

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+export default {
+  dialect: "postgresql",
+  schema: "./utils/schema.jsx",
+  out: "./drizzle",
+  dbCredentials: {
+    url: "postgresql://neondb_owner:npg_JiIVOMAb9HF4@ep-red-queen-a5pavpqh-pooler.us-east-2.aws.neon.tech/abc?sslmode=require",
+    connectionString:
+      "postgresql://neondb_owner:npg_JiIVOMAb9HF4@ep-red-queen-a5pavpqh-pooler.us-east-2.aws.neon.tech/abc?sslmode=require",
+  },
+};
